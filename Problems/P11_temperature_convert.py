58346082d8a085505104173c339c5792bf25d849
Kunal Koli <kunalkoli537@gmail.com>
2024-11-19 21:16:50 +0530

    Add files via upload

---
+++ Problems/P11_temperature_convert.py
@@ -0,0 +1,20 @@
+# To convert temperature from Fahrenheit to Celsius 
+
+# def f_to_c(f):
+#     return 5*(f-32)/9
+
+# f = int(input("Enter temperature in Fahrenheit: "))
+# print(f"Temperature in Celsius is: {round(f_to_c(f), 2)}")
+
+
+# Q. to prevent new line using "end"
+
+# print("a")
+# print("b")
+# print("c", end="")
+# print("d", end="")
+
+# o/p: 
+# a
+# b
+# cd   no new line is added because of "end"
