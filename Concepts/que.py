f58fcb5de1516ea9aabd58f2df02ec8a4fb271aa
Kunal Koli <kunalkoli537@gmail.com>
2024-11-25 16:34:16 +0530

    Add files via upload

---
+++ Concepts/que.py
@@ -0,0 +1,46 @@
+# Q1. take name of fruits from user and enter in list 
+
+# fruits = []
+
+# f1 = input("Enter name of fruit: ")
+# fruits.append(f1)
+# f2 = input("Enter name of fruit: ")
+# fruits.append(f2)
+# f3 = input("Enter name of fruit: ")
+# fruits.append(f3)
+# f4 = input("Enter name of fruit: ")
+# fruits.append(f4)
+
+# print(fruits)
+
+# Q.2 accept marks of student and displayin sorted manner
+
+# marks = []
+
+# f1 = int(input("Enter marks: "))
+# marks.append(f1)
+# f2 = int(input("Enter marks: "))
+# marks.append(f2)
+# f3 = int(input("Enter marks: "))
+# marks.append(f3)
+# f4 = int(input("Enter marks: "))
+# marks.append(f4)
+
+# marks.sort()
+# print(marks)
+
+# Q.3 sum 4 number in list 
+
+# li = (2,4,6,7)
+# print(sum(li))
+
+# Q.4 check no. of 0 in tuple 
+
+# t= (7,0,8,0,0,9)
+# a= t.count(0)
+# print(a)
+
+# Q5. check that tuple type cannot be changed in python 
+# a = (2,34.6,"apple")
+# a[2]= "mango"
+# TypeError: 'tuple' object does not support item assignment
