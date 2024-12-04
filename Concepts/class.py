f7eb3d3a9a1c1a26691ea1b9b43f570c6b811b9c
Kunal Koli <kunalkoli537@gmail.com>
2024-12-04 21:12:11 +0530

    Add files via upload

---
+++ Concepts/class.py
@@ -0,0 +1,8 @@
+# To define a class we use class keyword 
+
+class Employee:
+    language = "py"  # this is class attribute
+    salary = 12000
+
+k = Employee()    # k is an object 
+print(k.language, k.salary)
